838721c8dd47b32c13aaab300897406491d8486b
Tasklet Deploy <deploy@tasklet.ai>
2026-03-24 12:31:13 +0000

    v34: Fix OneSignal - use real SDK ref (window._OS) not window.OneSignal wrapper

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-const CACHE = 'vh-challenges-v33';
+const CACHE = 'vh-challenges-v34';
 const SHELL = ['./manifest.json'];
 
 // On install: cache non-HTML assets, skip waiting immediately
